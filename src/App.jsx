e37ff4dbb44c7678944bbcc148072babeb947bec
chintayanggest <chintacapric836@gmail.com>
2026-09-26 05:48:18 +0700

    fixing tap

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ function App() {
             </div>
 
             <button className="image-btn" onClick={() => setInputMode('text')}>
-                <img src="/public/WritingButton/gold.png" alt="✍️ Or write a message ➔" />
+              <img src="/public/WritingButton/gold.png" alt="✍️ Or write a message ➔" />
             </button>
           </>
         )}
